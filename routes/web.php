ed02e4df2b14e51c00271199b0b3f6822d72d19c
vanex97 <vanex97@gmail.com>
2022-03-16 12:30:18 +0200

    Updating routes for employees

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Auth::routes();
+
+Route::redirect('/', '/employees');
+
+Route::resource('employees', EmployeeController::class)
+    ->middleware('auth');
+
