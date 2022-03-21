5c9b412a2619e80f4d004b2d8c5a4f0bbc751aed
vanex97 <vanex97@gmail.com>
2022-03-21 18:44:34 +0200

    Adding admin scripts and styles to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+mix.js('resources/js/admin.js', 'public/js')
+    .js('resources/js/employeeForm.js', 'public/js')
+    .sass('resources/sass/admin.scss', 'public/css');
