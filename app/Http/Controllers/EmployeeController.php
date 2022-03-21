a4e4667579f4e176a9aa115542d0429a914955e7
vanex97 <vanex97@gmail.com>
2022-03-21 18:48:33 +0200

    Updating employee controller methods for create

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,42 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\DataTables\EmployeeDataTable;
+use App\Http\Requests\EmployeeRequest;
+use App\Models\Employee;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class EmployeeController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\JsonResponse
      */
-    public function index(Request $request, EmployeeDataTable $dataTable)
+    public function index(EmployeeDataTable $dataTable)
     {
-        if($request->ajax()) {
-            return $dataTable->ajax();
-        }
         return $dataTable->render('employee.index');
     }
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('employee.create');
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(EmployeeRequest $request)
     {
-        //
+        $employee = new Employee();
+        $employee->fill($request->except(['head', 'photo', 'phone_number']));
+        $employee->phone_number = phone($request->phone_number, 'UA', 'international');
+        $employee->photo = 'storage/'.$this->uploadEmployeePhoto($request);
+
+        if($request->has('head')) {
+            $employee->appendToNode(Employee::where('name', $request->head)->first());
+        }
+
+        $employee->save();
+
+        return redirect(route('employees.index'));
     }
 
     /**
@@ -84,5 +89,32 @@ class EmployeeController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function autocomplete(Request $request)
+    {
+        $name = $request->get('term');
+        $filterResult = Employee::where('name', 'LIKE', '%'. $name. '%')
+            ->select('name')
+            ->limit(5)
+            ->get();
+        return response()->json($filterResult);
+    }
+
+    /**
+     * @param Request $request
+     * @return string Image path
+     */
+    private function uploadEmployeePhoto(Request $request)
+    {
+        $image = Image::make($request->file('photo'))
+            ->fit(300, 300)
+            ->orientate()
+            ->stream('jpg', 80);
+
+        $imagePath = 'employee_photos/'.uniqid().'.jpg';
+        Storage::disk('public')->put($imagePath, $image);
+
+        return $imagePath;
     }
 }
