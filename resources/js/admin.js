934a2060b950f253507cad8516f4dc50cba37175
vanex97 <vanex97@gmail.com>
2022-03-21 18:41:46 +0200

    Adding admin script

---
+++ resources/js/admin.js
@@ -0,0 +1,10 @@
+require('./app');
+
+require('select2');
+
+import 'jquery-ui/ui/widgets/autocomplete.js';
+
+window.moment = require('moment')
+window.datetimepicker = require('tempusdominus-bootstrap-4');
+
+require( 'datatables.net-bs4' );
