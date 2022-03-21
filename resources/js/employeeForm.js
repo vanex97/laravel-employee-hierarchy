85fe72be5de51236118bc1ff39fb8acc95de7b06
vanex97 <vanex97@gmail.com>
2022-03-21 18:43:28 +0200

    Adding employeeForm script

---
+++ resources/js/employeeForm.js
@@ -0,0 +1,54 @@
+let CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+// Select 2 for position input.
+let $positionSelect = $('#position-select').select2({
+    theme: 'bootstrap4',
+    ajax: {
+        url: $('#position-select').attr('url'),
+        dataType: 'json',
+        type: "post",
+        delay: 250,
+        data:
+            function (params) {
+                return {
+                    _token: CSRF_TOKEN,
+                    search: params.term
+                };
+            },
+        processResults: function (response) {
+            return {
+                results: response
+            };
+        },
+        cache: true
+    }
+});
+// Autocomplete for head input.
+$("#head-input").autocomplete({
+    source: function (request, response) {
+        $.ajax({
+            url: $('#head-input').attr('url'),
+            data: {
+                term: request.term
+            },
+            dataType: "json",
+            success: function (data) {
+                var resp = $.map(data, function (obj) {
+                    return obj.name;
+                });
+                response(resp);
+            }
+        });
+    },
+    minLength: 2
+});
+// Datetimepicker for date input.
+$('#employment_date').datetimepicker({
+    format: 'DD/MM/YY'
+});
+// Format salary value before submit form.
+$('#employee_form').submit(function () {
+    // Remove commas from salary value
+    let $salary = $(this).find("input[name=salary]");
+    $salary.val($salary.val().replaceAll(',', ''));
+});
