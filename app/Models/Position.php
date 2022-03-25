98cca5de1ff46f28ff23eb3887b737be98630077
vanex97 <vanex97@gmail.com>
2022-03-25 13:11:25 +0200

    Updating Position model with booted and fillable

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -4,13 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Position extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name'];
+
     public function employees()
     {
         return $this->hasMany(Employee::class);
+    }
+
+    public static function booted()
+    {
+        static::creating(function($position) {
+            $position->admin_created_id = Auth::id();
+            $position->admin_updated_id = Auth::id();
+        });
+
+        static::updating(function($position) {
+            $position->admin_updated_id = Auth::id();
+        });
     }
 }
