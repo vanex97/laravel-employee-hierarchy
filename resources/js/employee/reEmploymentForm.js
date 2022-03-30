fd17b577b682f806b1ac12ac1d6e265e7bba3e0a
vanex97 <vanex97@gmail.com>
2022-03-30 20:04:42 +0300

    Updating reEmploymentForm.js

--- resources/js/employee/reEmploymentForm.js
+++ resources/js/employee/reEmploymentForm.js
@@ -21,3 +21,35 @@ $(".head-input").each(function () {
         minLength: 2
     });
 });
+$('#employee-table').DataTable({
+    autoWidth: false,
+    ordering: false,
+    searching: false,
+    paging: false,
+    info: false,
+    responsive: true,
+    columnDefs: [
+        { responsivePriority: 1, targets: 1 },
+        { responsivePriority: 2, targets: -1 }
+    ]
+});
+$('#confirm-button').click(function() {
+    let $deleteAlert = $('#delete-alert');
+    // Clear delete alert
+    $deleteAlert.empty();
+
+    // Get empty employee inputs
+    let deletedEmployees = [];
+    $('.head-input:empty').each(function () {
+        if (!$(this).val()) {
+            deletedEmployees.push($(this).attr('employee'));
+        }
+    });
+    // Set alert text
+    if (deletedEmployees.length) {
+        $deleteAlert.append('The following employees and their subordinates will be also removed:<br>')
+    }
+    deletedEmployees.forEach(function (element) {
+        $deleteAlert.append('<strong>'+element+'</strong><br>')
+    });
+})
